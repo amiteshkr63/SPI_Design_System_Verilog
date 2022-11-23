f710e3f07cdda98973820f77407cf33afcd39718
AMITESH KUMAR <88953654+amiteshkr63@users.noreply.github.com>
2022-11-23 18:19:13 +0530

    SCLK_working_properly
    IDLE_STATE: By default sending SPI_BUFF[7]
    Removed miso_data_counter using only mosi_data_counter as data_counter
    Removed LOAD state
    Issues remained to solve: 1) EDA playground needing 2 regs for storing data for extra 1 clks
                                             2) edge counter is of 5 bits which should be of 4 bits.

--- spi_master_v2.sv
+++ spi_master_v2.sv
@@ -20,17 +20,18 @@ reg [`WORD_LENGTH-1:0]SPI_BUFFER;
 reg  [$clog2(`CLK_PER_HALF_BIT*2)-1:0]SPI_CLK_count;
 reg [$clog2(`TOTAL_EDGE_COUNT):0]edge_counter;
 reg [$clog2(`WORD_LENGTH)-1:0]mosi_data_counter;
-reg [$clog2(`WORD_LENGTH)-1:0]miso_data_counter;
 reg trailing_edge;
 reg leading_edge;
 wire [1:0]SPI_MODE;
+reg r_apb_ready;
+reg r1_apb_ready;
 
 //CPOL and CPHA
 wire w_CPOL;
 wire w_CPHA;
 
 //States
-typedef enum {IDLE, LOAD, TRANSFER}states;
+typedef enum {IDLE, TRANSFER}states;
 states PST, NST;
 
 /**/////////////////////////////////////////////MODE SELECTION///////////////////////////////////////////**/																									//
@@ -40,17 +41,38 @@ states PST, NST;
 assign w_CPOL= (SPI_MODE==`MODE_POL_PHS_10) | (SPI_MODE==`MODE_POL_PHS_11);
 assign w_CPHA= (SPI_MODE==`MODE_POL_PHS_01) | (SPI_MODE==`MODE_POL_PHS_11);
 
+always_ff@(posedge clk or negedge rst_n) begin
+	if (!rst_n)	begin 
+		r_apb_ready<=0;
+		r1_apb_ready<=0;
+	end
+	else begin
+		r1_apb_ready<=r_apb_ready;
+		r_apb_ready<=apb_ready;
+	end
+end
+
 //SCLK Clock Handling
 always_ff@(posedge clk or negedge rst_n) begin
-	if((!rst_n) || (PST==IDLE)) begin
-		SCLK<=w_CPOL;					//When rst_n SCLK is in default state
+	if((!rst_n) || (PST==IDLE)  || ((edge_counter=='d0) && (apb_ready || r1_apb_ready) && (PST==TRANSFER)) ) begin
+		SCLK<=w_CPOL;									//When rst_n SCLK is in default state
 		SPI_CLK_count<=0;
 		edge_counter<=`TOTAL_EDGE_COUNT;	
 		leading_edge<=0;
 		trailing_edge<=0;
 	end
 	else begin
-		if (edge_counter>0) begin						//Leading Edge Detection
+/*		if (mosi_data_counter=='d7) begin
+			edge_counter<=`TOTAL_EDGE_COUNT-'d1;
+			if (apb_ready) begin
+				SCLK=~SCLK;
+				SPI_CLK_count<=SPI_CLK_count+'b1;
+				edge_counter<=`TOTAL_EDGE_COUNT-'d1;
+				leading_edge<=~w_CPHA;
+				trailing_edge<=w_CPHA;		
+			end
+		end
+		else */if (edge_counter>0) begin						//Leading Edge Detection
 			if (SPI_CLK_count == (`CLK_PER_HALF_BIT -'b1)) begin
 				SCLK<=~SCLK;
 				SPI_CLK_count<=SPI_CLK_count+'b1;
@@ -102,31 +124,31 @@ always_comb begin
 		IDLE: 
 			begin	
 				if (apb_ready) begin
-					NST=LOAD;
+					NST=TRANSFER;
 				end
 				else begin
 					NST=PST;
 				end 
 			end
-
-		LOAD:	
-			begin
-				if(SPI_status_RDY_BSYbar) NST=PST;
-				else NST=TRANSFER;
-			end
-
 		TRANSFER:
-				begin
-					if(((mosi_data_counter=='b0) && (SPI_CLK_count=='d3)))begin
-						if (apb_ready) begin
-							NST=LOAD;
-						end
-						else	NST=IDLE;
-					end
-					else begin
+			begin
+				if (!edge_counter && !(apb_ready || r1_apb_ready)) begin
+					NST=IDLE;
+				end
+				else NST=PST;
+			end
+/*		TRANSFER:
+			begin
+				if(!edge_counter)begin
+					if (apb_ready || r1_apb_ready) begin
 						NST=PST;
 					end
+					else	NST=IDLE;
 				end
+				else begin
+					NST=PST;
+				end
+			end*/
 	endcase
 end
 
@@ -136,45 +158,37 @@ always_comb begin
 		IDLE:
 			begin
 				if (!apb_ready) begin
-					{MOSI, SSbar} = {1'b0, `DISCONNECTED_FROM_SLAVE};
+					{MOSI, SSbar} = {SPI_BUFFER[`WORD_LENGTH-'d1], `DISCONNECTED_FROM_SLAVE};
 					SPI_status_RDY_BSYbar=`SPI_READY;
 					RDATA='b0;
 					SPI_BUFFER='b0;///////////////////////////////////////////////
 					rx_data_valid = 0;
 				end
 				else begin
-					{MOSI, SSbar} = {SPI_BUFFER[`WORD_LENGTH-'d1], `CONNECTED_FROM_SLAVE};
+					{MOSI, SSbar} = {SPI_BUFFER[`WORD_LENGTH-'d1], `DISCONNECTED_FROM_SLAVE};
 					SPI_status_RDY_BSYbar=`SPI_READY;
 					SPI_BUFFER=WDATA;///////////////////////////////////////////
 					RDATA='b0;
 					rx_data_valid = 0;
 				end
 			end
-
-		LOAD:
-			begin
-				{MOSI, SSbar} = {1'b0, `DISCONNECTED_FROM_SLAVE};
-				SPI_status_RDY_BSYbar=`SPI_BUSY;
-				SPI_BUFFER=WDATA;////////////////////////////
-				RDATA='b0;
-				rx_data_valid = 0;
-			end
 	
 		TRANSFER:
-				begin
-					if(((mosi_data_counter=='b0) && (SPI_CLK_count=='d3) && apb_ready))begin
-						SPI_BUFFER=WDATA;////////////////////////////////////////
-					end
-					else if ((leading_edge & w_CPHA) | (trailing_edge & ~w_CPHA)) begin
-						{MOSI, SSbar} = {SPI_BUFFER[mosi_data_counter], `CONNECTED_FROM_SLAVE};
-						SPI_status_RDY_BSYbar=`SPI_BUSY;
-						RDATA='b0;
-						rx_data_valid=0;
-					end	
-					else if((leading_edge & ~w_CPHA) | (trailing_edge & w_CPHA)) begin
-						SPI_BUFFER[mosi_data_counter]=MISO;
-					end
+			begin
+				SPI_status_RDY_BSYbar=`SPI_BUSY;
+				//if(((mosi_data_counter=='b0) && (SPI_CLK_count=='d3) && apb_ready))begin
+				if((!edge_counter) && (apb_ready))begin
+					SPI_BUFFER=WDATA;////////////////////////////////////////
 				end
+				else if ((leading_edge & w_CPHA) || (trailing_edge & ~w_CPHA)) begin
+					{MOSI, SSbar} = {SPI_BUFFER[mosi_data_counter], `CONNECTED_FROM_SLAVE};
+					RDATA='b0;
+					rx_data_valid=0;
+				end	
+				else if((leading_edge & ~w_CPHA) || (trailing_edge & w_CPHA)) begin
+					SPI_BUFFER[mosi_data_counter]=MISO;
+				end
+			end
 	endcase
 end
 endmodule
