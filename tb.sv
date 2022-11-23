973b750427d3f48a4c8553ccd64f0890f65f2d11
AMITESH KUMAR <88953654+amiteshkr63@users.noreply.github.com>
2022-11-23 18:19:36 +0530

    Update tb.sv

--- tb.sv
+++ tb.sv
@@ -25,42 +25,33 @@ spi_master inst_spi_master
 
 	initial	 begin
 		$dumpfile("dump.vcd"); $dumpvars;
-		{rst_n, clk, apb_ready, WDATA, MISO}=12'b0;
+      {rst_n, clk, apb_ready, WDATA, MISO}=12'b1;
 		#10 rst_n =1'b1;
 	end
 	
 	always #5 clk=~clk;
 
-  	always #20 MISO=~MISO;	
+  always @(negedge SCLK) MISO=~MISO;	
   
-      always begin
-        #50 {apb_ready, WDATA}=9'b1_1010_1010;
-        #10 apb_ready=1'b0;
+  always begin
+    #55 {apb_ready, WDATA}=9'b1_1010_1010;
+    #10 apb_ready=1'b0;
 		#320;
-        {apb_ready, WDATA}=10'b1_1010_1010;
-        #10 apb_ready=1'b0;
-        #320;
-        {apb_ready, WDATA}=10'b1_1010_1010;
-        #10 apb_ready=1'b0;
-        #320;
-        {apb_ready, WDATA}=10'b1_1010_1010;
-        #10 apb_ready=1'b0;
-        #320;
-        {apb_ready, WDATA}=10'b1_1010_1010;
-        #10 apb_ready=1'b0;
-        #320;
-        {apb_ready, WDATA}=10'b1_1010_1010;
-        #10 apb_ready=1'b0;
-        #320;
-        {apb_ready, WDATA}=10'b1_1010_1010;
-        #10 apb_ready=1'b0;
-        #320;
-		{apb_ready, WDATA}=10'b1_1010_1010;
-        #10 apb_ready=1'b0;
-        #320;
-        {apb_ready, WDATA}=10'b1_1010_1010;
-        #10 apb_ready=1'b0;
-        #320;
-    end	
+		{apb_ready, WDATA}=9'b1_1010_1010;
+		#10 apb_ready=1'b0;
+		#320;
+		{apb_ready, WDATA}=9'b1_1010_1010;
+		#10 apb_ready=1'b0;
+		#320;
+		{apb_ready, WDATA}=9'b1_1010_1010;
+		#10 apb_ready=1'b0;
+		#320;
+		{apb_ready, WDATA}=9'b1_1010_1010;
+		#10 apb_ready=1'b0;
+		#320;
+		{apb_ready, WDATA}=9'b1_1010_1010;
+		#10 apb_ready=1'b0;
+		#320;
+end	
   
 endmodule
